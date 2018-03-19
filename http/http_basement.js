99748a36e3c43783566b17c3ab805ad5c9e32d53
Abise <1097843969@qq.com>
2018-03-20 00:32:48 +0800

    :star: Fullfill the http class part

--- http/http_basement.js
+++ http/http_basement.js
@@ -55,56 +55,56 @@ var http = require('http')
 // 以下是客户端和服务端的演示
 
 // const http = require('http');
-const net = require('net');
-const url = require('url');
+// const net = require('net');
+// const url = require('url');
 
-// 创建一个HTTP代理服务器
-const proxy = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
-})
-proxy.on('connect', (req, cltSocket, head) => {
-    // 连接一个服务器
-    const srvUrl = url.parse(`http://${req.url}`);
-    const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
-        cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-        'Proxy-agent: Node.js-Proxy\r\n' +
-        '\r\n');
-        srvSocket.write(head);
-        srvSocket.pipe(cltSocket);
-        cltSocket.pipe(srvSocket);
-    })
-});
+// // 创建一个HTTP代理服务器
+// const proxy = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('okay');
+// })
+// proxy.on('connect', (req, cltSocket, head) => {
+//     // 连接一个服务器
+//     const srvUrl = url.parse(`http://${req.url}`);
+//     const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
+//         cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+//         'Proxy-agent: Node.js-Proxy\r\n' +
+//         '\r\n');
+//         srvSocket.write(head);
+//         srvSocket.pipe(cltSocket);
+//         cltSocket.pipe(srvSocket);
+//     })
+// });
 
-// 代理服务器
-proxy.listen(1337, '127.0.0.1', () => {
+// // 代理服务器
+// proxy.listen(1337, '127.0.0.1', () => {
 
-    // 发送一个请求到代理服务器
-    const options = {
-        port: 1337,
-        hostname: '127.0.0.1',
-        method: 'CONNECT',
-        path: 'www.baidu.com:80'
-    };
+//     // 发送一个请求到代理服务器
+//     const options = {
+//         port: 1337,
+//         hostname: '127.0.0.1',
+//         method: 'CONNECT',
+//         path: 'www.baidu.com:80'
+//     };
 
-    const req = http.request(options);
-    req.end();
+//     const req = http.request(options);
+//     req.end();
 
-    req.on('connect', (res, socket, head) => {
-        console.log('Connected!?');
+//     req.on('connect', (res, socket, head) => {
+//         console.log('Connected!?');
 
-        socket.write('GET / HTTP/1.1\r\n' +
-        'Host: www.baidu.com:80\r\n' +
-        'Connection: close\r\n' +
-        '\r\n');
-        socket.on('data', (chunk) => {
-            console.log(chunk.toString());
-        });
-        socket.on('end', () => {
-            proxy.close();
-        });
-    });
-});
+//         socket.write('GET / HTTP/1.1\r\n' +
+//         'Host: www.baidu.com:80\r\n' +
+//         'Connection: close\r\n' +
+//         '\r\n');
+//         socket.on('data', (chunk) => {
+//             console.log(chunk.toString());
+//         });
+//         socket.on('end', () => {
+//             proxy.close();
+//         });
+//     });
+// });
 
 
 // countinue 服务器发送了 100 Continue 的响应时触发， 通常是因为请求包含Expect: 100-continue，这是客户端将要发送请求主体的指令
@@ -113,4 +113,90 @@ proxy.listen(1337, '127.0.0.1', () => {
 
 // socket 当socket被分配到请求后触发
 
-// 
+// timeout 事件 socket 超时的时候触发
+
+// upgrade  
+// 创建一个 HTTP 服务器
+const srv = http.createServer( (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('okay');
+  });
+  srv.on('upgrade', (req, socket, head) => {
+    socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+                 'Upgrade: WebSocket\r\n' +
+                 'Connection: Upgrade\r\n' +
+                 '\r\n');
+  
+    socket.pipe(socket);
+  });
+  
+  // 服务器正在运行
+  srv.listen(1337, '127.0.0.1', () => {
+  
+    // 发送一个请求
+    const options = {
+      port: 1337,
+      hostname: '127.0.0.1',
+      headers: {
+        'Connection': 'Upgrade',
+        'Upgrade': 'websocket'
+      }
+    };
+  
+    const req = http.request(options);
+    req.end();
+  
+    req.on('upgrade', (res, socket, upgradeHead) => {
+      console.log('got upgraded!');
+      socket.end();
+      process.exit(0);
+    });
+  });
+
+  // request.abort() 使响应中剩余的数据被丢弃且socket被损毁
+
+  // request.aborted 返回被终止的时间
+
+  // request.connection => request.socket
+
+  // request.end(data, ...encoding, cb)
+  // 结束发送请求
+
+  // request.flushHeader
+  // 刷新请求头， 通常会缓存请求头直到request.end()被调用或者第一块请求数据被写入，
+  // node 会将请求头和数据一并打包成单一的TCP数据包， 刷新可以提前开始请求1
+
+  // request。getHeader(name)  读出请求头 大小写敏感
+  const contentType = request.getHeader('Content-Type')
+
+  // request.removeHeader(name)  删除header内容
+
+  // request.setHeader(name, value) 为header设置单一的header值，有则会替换
+  request.setHeader('Content-Type', 'application/json');
+  request.setHeader('Set-Cookie', ['type=ninja', 'language=javascript']);
+
+  // request.setNoDelay([noDelay]) socket 被分配请求且已连接，socket.setNoDelay()会被调用
+
+  // request.setSocketKeepAlive(enable, [initialDelay])  socket 被分配请求且已连接, socket.setKeepAlive() 会调用
+
+  // request.setTimeout(timeout, ...callback) 触发条件同上
+
+  // request.socket 引用底层socket， request。end后为null，也可通过request.connection 访问socket
+  const options = {
+    host: 'nodejs.cn',
+  };
+  const req = http.get(options);
+  req.end();
+  req.once('response', (res) => {
+    const ip = req.socket.localAddress;
+    const port = req.socket.localPort;
+    console.log(`你的IP地址是 ${ip}，你的源端口是 ${port}。`);
+    // consume response object
+  });
+
+  // request.write(chunk, ...encoding, cb)
+  // 发送请求主体的一个数据块， 通过多次调用该方法， 一个请求主体可被发送到一个服务器，这种情况下，当创建请求时，建议使用['Transfer-Encoding', 'chunked']请求头
+
+
+  // http.Server 类
+  
